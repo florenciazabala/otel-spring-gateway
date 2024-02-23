889b8d4270299dadabd1d750b96e5d9151d01bd3
Florencia Agustina Zabala <fzabala@prismamp.com>
2024-02-22 23:40:44 -0300

    Initial commit

---
+++ src/test/java/com/demo/otelspringgateway/OtelSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.otelspringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelSpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
